602e56a037f2b1576f6512f617e9ca870ce75f2e
Chaithanya Kiran <kiran.chaithu@gmail.com>
2016-03-07 01:41:15 +0530

    Add layout with different widgets

--- Practice/Ckpractice/main.cpp
+++ Practice/Ckpractice/main.cpp
@@ -20,7 +20,7 @@ int main(int argc, char *argv[])
     //QObject::connect(button,SIGNAL(clicked()),&a,SLOT(quit()));
     //button->show();
 
-    QWidget *mainWindow = new QWidget;
+    /*QWidget *mainWindow = new QWidget;
     mainWindow->setWindowTitle("Hello CK");
 
     QSpinBox *spinner = new QSpinBox;
@@ -36,5 +36,23 @@ int main(int argc, char *argv[])
     layout->addWidget(spinner);
     mainWindow->setLayout(layout );
     mainWindow->show();
+    return a.exec();*/
+
+    QWidget *window = new QWidget;
+    QPushButton *button1 = new QPushButton("One");
+    QPushButton *button2 = new QPushButton("Two");
+    QPushButton *button3 = new QPushButton("Three");
+    QPushButton *button4 = new QPushButton("Four");
+    QPushButton *button5 = new QPushButton("Five");
+
+    QGridLayout *layout = new QGridLayout;
+    layout->addWidget(button1, 0, 0);
+    layout->addWidget(button2, 0, 1);
+    layout->addWidget(button3, 1, 0, 1, 2);
+    layout->addWidget(button4, 2, 0);
+    layout->addWidget(button5, 2, 1);
+
+    window->setLayout(layout);
+    window->show();
     return a.exec();
 }
